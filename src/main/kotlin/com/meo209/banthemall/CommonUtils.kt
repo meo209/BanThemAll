39c28088017dd684ccf74d002103c21ce9fe5596
meo209 <redslyice@outlook.de>
2024-09-21 00:34:45 +0200

    cleanup: CommonUtils

--- src/main/kotlin/com/meo209/banthemall/CommonUtils.kt
+++ src/main/kotlin/com/meo209/banthemall/CommonUtils.kt
@@ -13,14 +13,7 @@ object CommonUtils {
             ?.map { "${it.name}:${sha256Hash(it)}" }
             ?: emptyList()
 
-    fun sha256Hash(string: String): String {
-        val bytes = string.toByteArray()
-        val md = MessageDigest.getInstance("SHA256")
-        val digest = md.digest(bytes)
-        return digest.fold("") { str, it -> str + "%02x".format(it) }
-    }
-
-    fun sha256Hash(file: File): String {
+    private fun sha256Hash(file: File): String {
         val bytes = Files.readAllBytes(Paths.get(file.absolutePath))
         val md = MessageDigest.getInstance("SHA256")
         val digest = md.digest(bytes)
